1a570f9e4f0b1bf410c43e2f05e497a46ec55c08
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-10 22:39:56 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :start_time
-      t.datetime :closing_time
+      t.string :start_time
+      t.string :end_time
       t.timestamps
     end
   end
