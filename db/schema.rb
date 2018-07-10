0e28fdb3fc012feccd9388b46e7273a9c7d66fa6
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-10 22:47:09 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.text "opening_time"
-    t.text "closing_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
