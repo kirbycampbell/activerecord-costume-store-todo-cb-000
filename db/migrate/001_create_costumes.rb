8a4da85875f5ec06ca6679d2357e1e08beacc9e8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-10 22:21:15 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,4 +10,12 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.string :size
+      t.text :image_url
+    end
+  end
 end
