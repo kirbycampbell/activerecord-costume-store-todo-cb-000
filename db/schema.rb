e90a840ecea0d1ed02cd156bac73f6766e534712
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-10 22:22:24 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "price"
     t.string "size"
     t.text "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
