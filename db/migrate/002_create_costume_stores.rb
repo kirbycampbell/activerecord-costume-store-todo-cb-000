ec6c3b7f092f603be5d37a3d564f1abbd7696517
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-10 22:36:28 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
+      t.integer :num_of_employees
+      t.bool :still_in_business
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.integer "costume_inventory"
+    t.integer "number_of_employees"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
